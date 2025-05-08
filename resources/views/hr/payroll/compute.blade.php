d59228d658c69d3b8585dd0659e9517b714d064a
devwithcha <chacator.dev@gmail.com>
2025-05-08 23:00:09 +0800

    Salary Computation - DONE

--- resources/views/hr/payroll/compute.blade.php
+++ resources/views/hr/payroll/compute.blade.php
@@ -17,11 +17,11 @@
             <div class="card-body">
                 <form action="" method="POST">
                     @csrf
-
                     <input type="hidden" name="employee_id" value="{{ $employee->id }}">
                     <input type="hidden" name="cutoff" value="{{ $cutoff }}">
                     <input type="hidden" name="month" value="{{ $month }}">
 
+                    {{-- Header --}}
                     <div class="row mb-3">
                         <div class="col-md-4">
                             <label>Employee ID</label>
@@ -34,14 +34,12 @@
                         <div class="col-md-4">
                             <label>Month</label>
                             <input type="text" class="form-control" value="{{ \Carbon\Carbon::createFromFormat('Y-m', $month)->format('F Y') }}" readonly>
-
                         </div>
                     </div>
 
                     <hr>
+                    <h5 class="text-primary">Rate Details</h5>
 
-                                    
-                    <!-- Row 1: Basic Info -->
                     <div class="row mb-3">
                         <div class="col-md-3">
                             <label>Basic Pay</label>
@@ -49,48 +47,71 @@
                         </div>
                         <div class="col-md-3">
                             <label>Rate/Day</label>
-                            <input type="number" step="0.01" id="ratePerDay" name="rate_per_day" class="form-control" readonly>
+                            <input type="number" id="ratePerDay" name="rate_per_day" class="form-control" readonly>
                         </div>
                         <div class="col-md-3">
-                            <label>15-Day Pay</label>
-                            <input type="number" step="0.01" id="halfMonthPay" name="half_month_pay" class="form-control" readonly>
+                            <label>Rate/Hour</label>
+                            <input type="number" name="rate_per_hour" class="form-control" readonly>
                         </div>
+                        <div class="col-md-3">
+                            <label>Rate/OTS</label>
+                            <input type="number" name="rate_ots" class="form-control" readonly>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-3">
+                            <label>15-Day Pay</label>
+                            <input type="number" id="halfMonthPay" name="half_month_pay" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>% of Hourly Rate</label>
+                            <input type="number" name="percent_hourly_rate" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>SH Rate</label>
+                            <input type="number" name="sh_rate" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>RH Rate</label>
+                            <input type="number" name="rh_rate" class="form-control" readonly>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <label>Rest Day Rate</label>
+                            <input type="number" name="restday_rate" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>RND</label>
+                            <input type="number" name="rnd" class="form-control" readonly>
+                        </div>
+                    </div>
+
+                    <hr>
+                    <h5 class="text-success">Actual Computation</h5>
+
+                    <div class="row mb-3">
                         <div class="col-md-3">
                             <label>Allowance</label>
                             <input type="number" name="allowance" class="form-control">
                         </div>
-                    </div>
-
-                    <!-- Row 2: Adjustments & Attendance -->
-                    <div class="row mb-3">
                         <div class="col-md-3">
                             <label>Adjusted OT</label>
                             <input type="number" name="adjusted_ot" class="form-control">
                         </div>
                         <div class="col-md-3">
                             <label>Days Absent</label>
-                            <input type="number" name="days_absent" class="form-control" value="{{ $daysAbsent ?? 0 }}" readonly>
+                            <input type="number" name="days_absent" class="form-control" value="{{ $daysAbsent ?? 0 }}">
                         </div>
                         <div class="col-md-3">
-                            <label>Leave Hours / Tardiness</label>
-                            <input type="number" name="leave_hours" class="form-control" value="{{ $totalLateMinutes ?? 0 }}" readonly>
-                        </div>
-                        <div class="col-md-3">
-                            <label>RND</label>
-                            <input type="number" name="rnd" class="form-control">
+                            <label>Tardiness (Minutes)</label>
+                            <input type="number" name="tardiness" class="form-control" value="{{ $totalLateMinutes ?? 0 }}">
                         </div>
                     </div>
 
-                    <!-- Row 3: Rates -->
                     <div class="row mb-3">
-                        <div class="col-md-3">
-                            <label>Rate/Hour</label>
-                            <input type="number" name="rate_per_hour" class="form-control">
-                        </div>
-                        <div class="col-md-3">
-                            <label>% of Hourly Rate</label>
-                            <input type="number" name="percent_hourly_rate" class="form-control">
-                        </div>
                         <div class="col-md-3">
                             <label>Restday Pay</label>
                             <input type="number" name="restday_pay" class="form-control">
@@ -99,10 +120,6 @@
                             <label>Special Holiday Pay</label>
                             <input type="number" name="special_holiday_pay" class="form-control">
                         </div>
-                    </div>
-
-                    <!-- Row 4: Overtime -->
-                    <div class="row mb-3">
                         <div class="col-md-3">
                             <label>Regular OT Pay</label>
                             <input type="number" name="reg_ot_pay" class="form-control">
@@ -111,6 +128,9 @@
                             <label>RH OT Pay</label>
                             <input type="number" name="rh_ot_pay" class="form-control">
                         </div>
+                    </div>
+
+                    <div class="row mb-3">
                         <div class="col-md-3">
                             <label>SH OT Pay</label>
                             <input type="number" name="sh_ot_pay" class="form-control">
@@ -118,6 +138,25 @@
                         <div class="col-md-3">
                             <label>Total OT Pay</label>
                             <input type="number" name="total_ot" class="form-control">
+                        </div>
+                        <div class="col-md-3">
+                            <label>Pag-IBIG</label>
+                            <input type="number" name="pagibig" class="form-control" value="0">
+                        </div>
+                        <div class="col-md-3">
+                            <label>SSS</label>
+                            <input type="number" name="sss" class="form-control" value="0">
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-md-3">
+                            <label>Tardiness Deduction</label>
+                            <input type="number" name="tardiness_deduction" class="form-control" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label>Total Salary</label>
+                            <input type="number" name="total_salary" class="form-control" readonly>
                         </div>
                     </div>
 
@@ -132,22 +171,56 @@
 
     </div>
 </div>
-
 <script>
     function calculateDerivedFields() {
         const basicPay = parseFloat(document.querySelector('[name="basic_pay"]').value) || 0;
+        const pagibig = parseFloat(document.querySelector('[name="pagibig"]').value) || 0;
+        const sss = parseFloat(document.querySelector('[name="sss"]').value) || 0;
+        const allowance = parseFloat(document.querySelector('[name="allowance"]').value) || 0;
+        const restdayPay = parseFloat(document.querySelector('[name="restday_pay"]').value) || 0;
+        const shPay = parseFloat(document.querySelector('[name="special_holiday_pay"]').value) || 0;
+        const rhPay = parseFloat(document.querySelector('[name="rh_ot_pay"]').value) || 0;
+        const regOt = parseFloat(document.querySelector('[name="reg_ot_pay"]').value) || 0;
+        const shOt = parseFloat(document.querySelector('[name="sh_ot_pay"]').value) || 0;
+        const adjustedOt = parseFloat(document.querySelector('[name="adjusted_ot"]').value) || 0;
+        const tardinessMinutes = parseFloat(document.querySelector('[name="tardiness"]').value) || 0;
+        const daysAbsent = parseFloat(document.querySelector('[name="days_absent"]').value) || 0;
+
         const ratePerDay = (basicPay * 12) / 261;
+        const ratePerHour = ratePerDay / 8;
         const halfMonthPay = basicPay / 2;
+
+        const percentOfHourly = ratePerHour * 0.25;
+        const rndRate = ratePerHour * 0.10;
+        const shRate = ratePerDay * 0.30;
+        const rhRate = ratePerHour * 2.6;
+        const rateOTS = (ratePerDay * 2) / 8;
+        const restDayRate = ratePerDay + shRate;
+
+        const tardinessDeduction = (ratePerDay / 480) * tardinessMinutes;
+        const absentDeduction = daysAbsent * ratePerDay;
+
+        const totalEarnings = halfMonthPay + allowance + restdayPay + shPay + rhPay + regOt + shOt + adjustedOt;
+        const totalDeductions = tardinessDeduction + absentDeduction + pagibig + sss;
+        const totalSalary = totalEarnings - totalDeductions;
 
         document.getElementById('ratePerDay').value = ratePerDay.toFixed(2);
         document.getElementById('halfMonthPay').value = halfMonthPay.toFixed(2);
+        document.querySelector('[name="rate_per_hour"]').value = ratePerHour.toFixed(2);
+        document.querySelector('[name="percent_hourly_rate"]').value = percentOfHourly.toFixed(2);
+        document.querySelector('[name="rnd"]').value = rndRate.toFixed(2);
+        document.querySelector('[name="sh_rate"]').value = shRate.toFixed(2);
+        document.querySelector('[name="rh_rate"]').value = rhRate.toFixed(2);
+        document.querySelector('[name="rate_ots"]').value = rateOTS.toFixed(2);
+        document.querySelector('[name="restday_rate"]').value = restDayRate.toFixed(2);
+        document.querySelector('[name="tardiness_deduction"]').value = tardinessDeduction.toFixed(2);
+        document.querySelector('[name="total_salary"]').value = totalSalary.toFixed(2);
     }
 
     document.addEventListener('DOMContentLoaded', () => {
-        const basicPayInput = document.querySelector('[name="basic_pay"]');
-        basicPayInput.addEventListener('input', calculateDerivedFields);
-
-        // Run on page load in case of pre-filled values
+        document.querySelectorAll('input').forEach(input => {
+            input.addEventListener('input', calculateDerivedFields);
+        });
         calculateDerivedFields();
     });
 </script>
